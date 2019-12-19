f13f720b08a54ce3a81dd98395426d4d90488d2a
Lakshmi Narayanan <Lakshmi Narayanan@DESKTOP-J9HUCKP>
2019-12-19 07:40:51 +0530

    Lakshmi Changes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values (1,'Lakshmi','Java');
+insert into alien values (2,'Jeya','J2EE');
+insert into alien values (3,'Sahana','Java');
+insert into alien values (4,'Harshiv','JPA');
+insert into alien values (5,'Narayanan','Hibernate');
+insert into alien values (6,'Kiruthik','Spring');
